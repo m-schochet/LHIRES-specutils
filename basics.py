d4948fc8280752833597aa261122442b690a9022
Meir Schochet <103089439+m-schochet@users.noreply.github.com>
2024-11-26 19:33:38 -0500

    Update basics.py

--- basics.py
+++ basics.py
@@ -157,7 +157,7 @@ def plotter(obj_image, obj_name, manual_vscales=None, obj_type="detector-direct"
             ax.set_xlabel("Wavelength (" + unit + ")", fontsize=10)
             ax.set_ylabel("Intensity", fontsize=10)
 
-def tracer(obj_image, min_y, max_y, model, npix, vmin, vmax, aspect=0, npix_bot=None, hot_pix_min_cut=None, hot_pix_max_cut=None, plot_cutouts=False):
+def tracer(obj_image, min_y, max_y, model, npix, vmin, vmax, xlims=None, aspect=0, npix_bot=None, hot_pix_min_cut=None, hot_pix_max_cut=None, plot_cutouts=False):
      
     """
     This function is meant to help us determine the trace of our object. After running this function, the trace is plotted, and so if there are
@@ -228,7 +228,13 @@ def tracer(obj_image, min_y, max_y, model, npix, vmin, vmax, aspect=0, npix_bot=
                 bad_pixels = (weighted_yaxis_values < hot_pix_min_cut)
         elif (hot_pix_max_cut != None):
             bad_pixels = (weighted_yaxis_values > hot_pix_max_cut)
-        
+
+        if((xlims!=None) & (type(xlims)==tuple)):
+            xmin = xlims[0]
+            xmax = xlims[1]
+            bad_pixels = bad_pixels[xmin:xmax]
+            xvals = xvals[xmin:xmax]
+            
         fit_model = linfitter(model, xvals[~bad_pixels], weighted_yaxis_values[~bad_pixels])
         print(fit_model)
         
@@ -263,6 +269,11 @@ def tracer(obj_image, min_y, max_y, model, npix, vmin, vmax, aspect=0, npix_bot=
         return fit_model, mean_trace_profile, xvals, weighted_yaxis_values, npix_ret, bad_pixels
     
     else:
+        if((xlims!=None) & (type(xlims)==tuple)):
+            xmin = xlims[0]
+            xmax = xlims[1]
+            xvals = xvals[xmin:xmax]
+            
         fit_model = linfitter(model, xvals, weighted_yaxis_values)
         print(fit_model)
         
