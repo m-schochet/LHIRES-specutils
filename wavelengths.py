ed53d2faf9e60167fb7af6981539df2b31cd5c4a
Meir Schochet <103089439+m-schochet@users.noreply.github.com>
2024-11-28 12:21:08 -0500

    Update wavelengths.py
    added ability to fit neon+argon in the wl fitting function

--- wavelengths.py
+++ wavelengths.py
@@ -14,8 +14,9 @@ def initial_wl(calibration_spectra, xlims, bad_pixel_mask, guess_pixels, guess_w
   """
   This function is meant to help us get an initial feel for the wavelength solution for a calibration spectra when given a set of guessed pixels of lines and associated wavelengths of those lines
 
+  *Note, since this function simply takes *guesses* and turns them into a solution, one can provide pixel/wavelength pairs for non-Neon lines too*
 
-  Inputs (6, 7 if needed):
+  Inputs (6, 8 if needed):
         calibration_spectra: (np.ndarray) the spectra of a calibration lamp (assumed that it was made using spectra.spectra_producer)
         
         xlims: (tuple) a set of xmin/xmax values to create an x-axis with
@@ -57,7 +58,7 @@ def initial_wl(calibration_spectra, xlims, bad_pixel_mask, guess_pixels, guess_w
   return wavelengths, linear_fit_wlmodel
 
 
-def wavelength_solver(spectra, xlims, bad_pixel_mask, initial_wl_soln, fit_model, guess_wl, guess_pixels, intensity_scaling = 2):
+def wavelength_solver(spectra, xlims, bad_pixel_mask, initial_wl_soln, fit_model, guess_wl, guess_pixels, intensity_scaling = 2, argon=False, argon_wls=[], argon_pixels=[]):
   """
   This function is meant to help us get a true wavelength solution after doing initial fitting using guessed pixels-wavelength pairs
 
@@ -65,7 +66,7 @@ def wavelength_solver(spectra, xlims, bad_pixel_mask, initial_wl_soln, fit_model
   and simply uses those to fit a new wavelength solution. This avoids the lists being incorrect sizes and having errors at every step.
 
 
-  Inputs (7, 8 if needed):
+  Inputs (7, 11 if needed):
         spectra: (np.ndarray) the spectra of a calibration lamp (assumed that it was the same used in using wavelengths.intial_wl
         
         xlims: (tuple) a set of xmin/xmax values to create an x-axis with
@@ -75,11 +76,18 @@ def wavelength_solver(spectra, xlims, bad_pixel_mask, initial_wl_soln, fit_model
         initial_wl_soln: this is the returned wavelength solution from the wavelengths.intial_wl function
 
         fit_model: this is the returned fit model from the wavelengths.intial_wl function
+        
         guess_wl: (list) the corresponding wavelengths that line up with the integer pixel values from the guess_pixels list
 
         guess_pixels: (list) a set of guessed pixels (MUST BE integers) in the calibration image
         
         intensity_scaling: (int) a scaling factor for the displayed neon lines from NIST, larger values plot them at larger y-axis values (default is 2)
+
+        argon: (bool) set to True if you also want to fit a set of Argon lines into the spectra as well
+
+        argon_wls: (list) list of wavelengths of Argon lines if argon is set to True
+
+        argon_pixels: (list) list of pixels of associated Argon lines if argon is set to True
   Returns (1):
         fit_model_with_true_neon: corrected model which uses the NIST lists
         
@@ -121,20 +129,78 @@ def wavelength_solver(spectra, xlims, bad_pixel_mask, initial_wl_soln, fit_model
                               y=waves_ne_guess)
   wavelength_model = fit_model_with_true_neon(xaxis[~bad_pixel_mask]) * u.AA
 
-  print("Original Fit\n" + str(fit_model) + "\n")
-  print("Fit Using NIST as Well\n" + str(fit_model_with_true_neon) + "\n")
+  if(argon==True):
+    argon_lines = Nist.query(minwav=minwave,
+                            maxwav=maxwave,
+                            wavelength_type='vac+air',
+                            linename='Ar I')
     
-  fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(14,14))
-  ax1.plot(initial_wl_soln, spectra)
-  ax1.plot(ne_keep_final, ne_rel_intens*intensity_scaling, 'x')
-  ax1.set_ylabel("Intensity")
-  ax1.set_xlabel("Wavelength ($\AA$)")
+    ar_keep = (argon_lines['Rel.'] != "*") &  (~argon_lines['Rel.'].mask)
+    ar_wl_only_good = argon_lines['Observed'][ar_keep]
+    ar_rel_only_good = np.array([float(x) for x in argon_lines['Rel.'][ar_keep]])
+          
+    wavelengths2 = np.array(ar_wl_only_good)
+    df2 = pd.DataFrame(wavelengths2)
+          
+    used_for_guesses2 = []
     
-  ax2.plot(wavelength_model, spectra)
-  ax2.vlines(ne_keep_final, np.min(spectra), np.max(spectra), 'r', alpha=0.45, linestyle='--')
-  for wl in ne_keep_final:
-      plt.text(wl+4, np.max(spectra)-np.std(spectra), str(wl) +"$\AA$", rotation=90, ha='right', va='top')
-  ax2.set_ylim(np.min(spectra), np.max(spectra));
-  ax2.set_xlabel("Wavelength ($\AA$)");
-  ax2.set_title("Calibration Neon Lamp")
-  return fit_model_with_true_neon
+    for i in range(len(argon_wls)):
+        checker2 = argon_wls[i]
+        checked2 = np.abs((df2 - checker2))
+        val2 = checked.loc[checked2[0] == np.min(checked2)].index[0]
+        used_for_guesses2.append(val2)
+    
+    ar_keep_final = np.asarray(ar_wl_only_good[used_for_guesses2]).tolist()
+    
+    ar_rel_only_good = ar_rel_only_good[used_for_guesses2]
+    ar_rel_intens = (ar_rel_only_good / ar_rel_only_good.max() * spectra.max())
+    
+    ar_pixel_vals = np.asarray(fit_model_1303.inverse(ar_keep_final)).tolist()
+    
+    xvals_ar_guess = np.concatenate([guesspix, ne_pixel_vals, ar_pixel_vals])
+    waves_ar_guess = np.concatenate([guess_wl, ne_keep_final, ar_keep_final])
+      
+    fit_model_with_argon_neon = linfitter(model=wlmodel,
+                                x=xvals_ar_guess,
+                                y=waves_ar_guess)
+    wavelength_model = fit_model_with_argon_neon(xaxis[~bad_pix_1303]) * u.AA
+    print("Original Fit\n" + str(fit_model) + "\n")
+    print("Fit Using NIST (+Argon) as Well\n" + str(fit_model_with_argon_neon) + "\n")
+    
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(14,14))
+    ax1.plot(initial_wl_soln, spectra)
+    ax1.plot(ne_keep_final, ne_rel_intens*intensity_scaling, 'x', label="Neon")
+    ax1.plot(ar_keep_final, ar_rel_intens*intensity_scaling, '+', label="Argon")
+    ax1.set_ylabel("Intensity")
+    ax1.set_xlabel("Wavelength ($\AA$)")
+      
+    ax2.plot(wavelength_model, spectra)
+    ax2.vlines(ar_keep_final, np.min(spectra), np.max(spectra), 'orange', alpha=0.25, linestyle='--', label="Argon")
+    ax2.vlines(ne_keep_final, np.min(spectra), np.max(spectra), 'r', alpha=0.25, linestyle='--', label="Neon")
+    for wl in ne_keep_final:
+        plt.text(wl+4, np.max(spectra)-np.std(spectra), str(wl) +"$\AA$", rotation=90, ha='right', va='top')
+    for wl2 in ar_keep_final:
+        plt.text(wl2+4, np.max(spectra)-np.std(spectra), str(wl) +"$\AA$", rotation=90, ha='right', va='top')
+    
+    ax2.set_ylim(np.min(spectra), np.max(spectra));
+    ax2.set_xlabel("Wavelength ($\AA$)");
+    ax2.set_title("Calibration Neon Lamp")
+    return fit_model_with_argon_neon
+  else:
+    print("Original Fit\n" + str(fit_model) + "\n")
+    print("Fit Using NIST as Well\n" + str(fit_model_with_true_neon) + "\n")
+      
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(14,14))
+    ax1.plot(initial_wl_soln, spectra)
+    ax1.plot(ne_keep_final, ne_rel_intens*intensity_scaling, 'x')
+    ax1.set_ylabel("Intensity")
+    ax1.set_xlabel("Wavelength ($\AA$)")
+      
+    ax2.plot(wavelength_model, spectra)
+    ax2.vlines(ne_keep_final, np.min(spectra), np.max(spectra), 'r', alpha=0.45, linestyle='--')
+    for wl in ne_keep_final:
+        plt.text(wl+4, np.max(spectra)-np.std(spectra), str(wl) +"$\AA$", rotation=90, ha='right', va='top')
+    ax2.set_ylim(np.min(spectra), np.max(spectra));
+    ax2.set_xlabel("Wavelength ($\AA$)");
+    ax2.set_title("Calibration Neon Lamp")
+    return fit_model_with_true_neon
